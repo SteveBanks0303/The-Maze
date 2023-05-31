f235a4e3a1713a629bc538cc1ae34fd8f19e47de
SteveBanks0303 <111028784+SteveBanks0303@users.noreply.github.com>
2023-05-31 01:43:00 +0100

    Update map.c

--- src/map.c
+++ src/map.c
@@ -17,20 +17,8 @@ static const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 };
 
 /**
- * isInsideMap - check if we continue within the map
- * @x: next x coordinate
- * @y: next y coordinate
- * @Return: true if it is within the map, false otherwise
-*/
-
-bool isInsideMap(float x, float y)
-{
-	return (x >= 0 && x <= MAP_NUM_COLS * TILE_SIZE &&
-		y >= 0 && y <= MAP_NUM_ROWS * TILE_SIZE);
-}
-
-/**
  * DetectCollision - Checks if there could be a collision
+ * with the wall in the next player advance
  * @x: next x coordinate
  * @y: next y coordinate
  * Return: true if collision is detected, false otherwise
@@ -41,7 +29,7 @@ bool DetectCollision(float x, float y)
 	int mapGridX, mapGridY;
 
 	if (x < 0 || x >= MAP_NUM_COLS * TILE_SIZE ||
-		y < 0 || y >= MAP_NUM_ROWS * TILE_SIZE)
+			y < 0 || y >= MAP_NUM_ROWS * TILE_SIZE)
 		return (true);
 
 	mapGridX = floor(x / TILE_SIZE);
@@ -50,11 +38,25 @@ bool DetectCollision(float x, float y)
 }
 
 /**
+ * isInsideMap - check if we continue within the map
+ * @x: next x coordinate
+ * @y: next y coordinate
+ * @Return: true if it is within the map, false otherwise
+*/
+
+bool isInsideMap(float x, float y)
+{
+	return (x >= 0 && x <= MAP_NUM_COLS * TILE_SIZE &&
+				y >= 0 && y <= MAP_NUM_ROWS * TILE_SIZE);
+}
+
+/**
  * getMapValue - check if we continue within the map
  * @row: map row to check
  * @col: map column to check
  * @Return: The position value in the map
 */
+
 int getMapValue(int row, int col)
 {
 
@@ -64,6 +66,7 @@ int getMapValue(int row, int col)
 
 /**
  * renderMap - render the map
+ *
 */
 
 void renderMap(void)
@@ -79,11 +82,11 @@ void renderMap(void)
 			tileY = i * TILE_SIZE;
 			tileColor = map[i][j] != 0 ? 0xFFFFFFFF : 0x00000000;
 
-			drawRectangle(
-				tileX* MINIMAP_SCALE_FACTOR,
-				tileY *MINIMAP_SCALE_FACTOR,
-				TILE_SIZE* MINIMAP_SCALE_FACTOR,
-				TILE_SIZE *MINIMAP_SCALE_FACTOR,
+			drawRect(
+				tileX * MINIMAP_SCALE_FACTOR,
+				tileY * MINIMAP_SCALE_FACTOR,
+				TILE_SIZE * MINIMAP_SCALE_FACTOR,
+				TILE_SIZE * MINIMAP_SCALE_FACTOR,
 				tileColor
 			);
 		}
